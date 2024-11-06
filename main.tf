59c33daa5a8a9da72b0b3cd896a6ba36ce06dcbd
206675896_nbcuni <priyabrata.jena@nbcuni.com>
2024-11-06 19:29:38 +0530

    dataflow

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "dataflow" {
+    source = "./module/df_resource",
+    project_id = "valid-verbena-437709-h5",
+    region = "us-central1",
+    zone= "us-central1-a",
+    bucket_name = "dataops-dataflow-2024",
+    topic_name = "dataops-dataflow-pub",
+    subscription_name = "dataops-dataflow-sub",
+    dataset_id = "dataflow",
+    table_conversations_name = "customer-conversations",
+    table_orders_name = "customer-orders"
+}
